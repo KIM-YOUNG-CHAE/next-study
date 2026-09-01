484b0f5013acb6df716464f36d6fe7cdc37c0949
KIM-YOUNG-CHAE <kyco199610@gmail.com>
2026-09-01 21:51:05 +0900

    넥스트 1일차

---
+++ app/basic/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+
+    // tailwind css
+
+    return (
+        <div>
+            <div className="w-[100px] h-[100px] bg-amber-300">box</div>
+            <div className="w-96 h-20 bg-blue-300 text-[#ff0000]">box2</div>
+        </div>
+    );
+}
---
+++ app/basic02/page.tsx
@@ -0,0 +1,9 @@
+export default function page() {
+    return (
+        <div className="flex flex-col gap-2 border-solid border-black border-2 items-center">
+            <div className="w-96 h-96 bg-orange-200">box01</div>
+            <div className="w-96 h-96 bg-orange-200">box02</div>
+            <div className="w-96 h-96 bg-orange-200">box03</div>
+        </div>
+    );
+}
